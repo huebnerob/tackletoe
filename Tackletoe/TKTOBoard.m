3fbe8e83b3e435f5788192305b2f8bec58625cae
GitMongo user <GitMongo email>
2013-07-07 11:52:44 -0400

    Finished checkWin

--- Tackletoe/TKTOBoard.m
+++ Tackletoe/TKTOBoard.m
@@ -35,8 +35,8 @@
                               @[@0,@3,@6] , @[@1,@4,@7] , @[@2,@5,@7] ,
                               @[@0,@4,@8] , @[@2,@4,@6] ];
 	
-	NSMutableArray *Xmarks = [NSMutableArray array];
-    NSMutableArray *Omarks = [NSMutableArray array];
+	NSMutableSet *Xmarks = [NSMutableSet set];
+    NSMutableSet *Omarks = [NSMutableSet set];
     
     for( int i = 0; i < [cells count]; i++ ) {
         switch ([cells[i] markedStatus]) {
@@ -54,6 +54,16 @@
         }
     }
     
+    int xWinPattern = -1;
+    int oWinPattern = -1;
+    for(int i = 0; i < [winPatterns count]; i++) {
+    	NSSet *pattern = [NSSet setWithArray:winPatterns[i]];
+    	if([pattern isSubset:Xmarks]) xWinPattern = i;
+    	if([pattern isSubset:Omarks]) oWinPattern = i;
+    }
+    
+    if(xWinPattern) return TKTOMarkedStatusX;
+    if(oWinPattern) return TKTOMarkedStatusO;
     return TKTOMarkedStatusUnmarked;
 }
 
@@ -61,13 +71,16 @@
     return [self checkWin];
 }
 
-/*
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
     // Drawing code
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    
 }
-*/
+
 
 @end
